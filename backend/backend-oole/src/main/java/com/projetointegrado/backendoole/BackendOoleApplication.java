1f2847a50e80b34fa5bb993a23bd3f811877ac11
Vitor Henrique <vhsouzaeng@gmail.com>
2020-02-27 16:25:37 -0300

    Esqueleto do backend criado

---
+++ backend/backend-oole/src/main/java/com/projetointegrado/backendoole/BackendOoleApplication.java
@@ -0,0 +1,13 @@
+package com.projetointegrado.backendoole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOoleApplication.class, args);
+	}
+
+}
---
+++ backend/backend-oole/src/test/java/com/projetointegrado/backendoole/BackendOoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegrado.backendoole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
