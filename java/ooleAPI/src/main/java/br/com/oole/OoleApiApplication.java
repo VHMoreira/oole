d96cbda4ebb525f99487838ff99386556630cbd0
Vitor Henrique Moreira <vhsouzaeng@gmail.com>
2020-03-22 11:05:19 -0300

    Primeiro commit

---
+++ java/ooleAPI/src/main/java/br/com/oole/OoleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.oole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoleApiApplication.class, args);
+	}
+
+}
---
+++ java/ooleAPI/src/test/java/br/com/oole/OoleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
