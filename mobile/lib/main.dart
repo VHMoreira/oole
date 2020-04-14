6eaf011c8611adb3edaed56ef6f7defca94000d3
Felipe Rafael <rafaelt.ibarbosa@gmail.com>
2020-04-14 20:58:43 -0300

    Update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -10,8 +10,11 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-
-      )
+        Image.asset("images/oole.jpg"
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+      ),
     );
   }
 
