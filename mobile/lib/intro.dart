0624c9da85eaa24874cfac8f828fa77bfcfcb43c
Felipe Rafael <rafaelt.ibarbosa@gmail.com>
2020-04-25 16:57:47 -0300

    Update intro.dart

--- mobile/lib/intro.dart
+++ mobile/lib/intro.dart
@@ -8,29 +8,43 @@ class Intro extends StatefulWidget {
 class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar: AppBar(
-        title: Text("Oolé app"),
-      ),
+    return Scaffold(
+      backgroundColor: Color(0xFF008140),
 
-      backgroundColor: Colors.green,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          
+          children: <Widget>[
+            Container(
+              child: Image.asset(
+                "images/inicio-app.png",
+                alignment: Alignment.center,
+                fit: BoxFit.fill,
+                height: 150.0,
+              ),
+            ),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        //mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container (
-            child: Image.asset(
-              "images/inicio-app.png",
-              alignment: Alignment.center,
-              fit: BoxFit.fill,
-              height: 150.0,
-            ), 
-          ),
+            Text(
+              "Publique vídeos de suas melhores\n" +
+                  "jogadas na melhor pataforma para\n" +
+                  "jogadores de futebol.",
+              style: TextStyle(color: Colors.white, fontSize: 16.0),
+            ),
 
-          Text("Ainda não possui uma conta cadastre-se é de graça!")
+            RaisedButton(
+              child: Text("Começar a usar!", style: TextStyle(color: Color(0xFF01E271), fontSize: 16.0),),
+              onPressed: () {},
+            ),
 
-        ],
+            RaisedButton(
+              child: Text("O que é?", style: TextStyle(color: Color(0xFF01E271), fontSize: 16.0),),
+              onPressed: () {},
+            ),
+
+          ],
+        ),
       ),
     );
   }
